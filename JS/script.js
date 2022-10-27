e1575213ee7cefa2c37d4141d2840ae6b6c2f255
Dankir0 <iiiautahmoptup@gmail.com>
2022-10-27 11:07:23 +1000

    Вернулся к проекту

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// Скрипт для смены темы
+const THEME = document.querySelector('.logo'); //выбираем кнопку для смены темы
+// Далее навешиваем эвент на кнопку.
+// При клике на кнопку выполняется функция, которая добавляет на body класс ".dark-theme".
+// При повторном нажатии - снимает класс. (toggle).
+THEME.addEventListener('click', function() {
+  document.body.classList.toggle('dark-theme');
+})
